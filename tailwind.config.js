e5b1480a985e75e1caa57c94a9d6e10b930716b4
Biswajit Das <dasb51844@gmail.com>
2026-03-10 21:34:40 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            boxShadow: {
+                '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+                '4xl': '0 50px 100px -20px rgba(0, 0, 0, 0.4)',
+            },
+            animation: {
+                'scan': 'scan 3s linear infinite',
+            },
+            keyframes: {
+                scan: {
+                    '0%': { top: '0%' },
+                    '100%': { top: '100%' },
+                }
+            }
+        },
     },
+
     plugins: [],
 }
